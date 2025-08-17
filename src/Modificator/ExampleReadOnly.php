f24dc4f6bd382ca1bc0675c743c8feebf5c96eff
Anton Panfilov <anton@panfilov.biz>
2025-08-16 19:17:17 -0700

    Attribute

--- src/Modificator/ExampleReadOnly.php
+++ src/Modificator/ExampleReadOnly.php
@@ -3,10 +3,12 @@
 namespace AP\OpenAPIPlus\Modificator;
 
 use AP\OpenAPIPlus\OpenAPIModificator;
+use Attribute;
 
 /**
  * Element won't add to example
  */
+#[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
 class ExampleReadOnly implements OpenAPIModificator
 {
     public function updateOpenAPIElement(array $spec): array
